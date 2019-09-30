09732d0e65bbced60efbece56d7daa7778517fa2
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-09-30 18:44:28 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,14 +32,18 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title
-  FROM characters
-  INNER JOIN series
-  ON characters.series_id = series.id
+  FROM series
+  INNER JOIN characters
+  ON series.author_id = characters.author_id
   GROUP BY series.title, species
   HAVING characters.species = 'human'
   ORDER BY COUNT(species) DESC
   LIMIT 1;"
+
+
 end
+
+
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(name)
